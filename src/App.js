e1bb55643af0267ef794b78e9d7e4b5b57a30f22
soumyajit-singha-roy <soumyajitsingharoy005@gmail.com>
2025-12-24 15:52:55 +0530

    Fix router for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from './pages/landing';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/landing";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/landing" element={<Landing />} />
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
+    </div>
   );
 }
 
